c1629c289a9a2733702dd4301f0404fe81ae7a34
Josh Sandquist <joshdsand@gmail.com>
2023-07-24 19:16:01 -0700

    created landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import { Box, Heading, Text, VStack } from '@chakra-ui/react';
 
 function Home() {
   return (
-    <header>
-      <h2>This is the Home Page</h2>
-    </header>
-  );
+    <VStack spacing={8} align="center"  p={8}>
+      {/* Content Box */}
+      <Box
+        color="white"
+        w={{ base: 'full', md: '4xl' }}
+        bg="blue.700"
+        borderRadius="lg"
+        boxShadow="md"
+        p={8} // Increased padding for more space inside the box
+      >
+        {/* Hero Section */}
+        <Box>
+          <Heading as="h1"  size="2xl" textAlign="center" mb={2}>Welcome to the Employee Tip Calculator</Heading>
+          <Text fontSize="xl" m={4} textAlign="center">The perfect tool for managing your employee tips and generating reports easily.</Text>
+        </Box>
+
+        {/* Features Section */}
+        <Box mt={2}>
+          <Heading as="h2" size="xl" textDecoration="underline" my={4}>Features</Heading>
+          <VStack align="start" spacing={2} mt={4}>
+            <Box>
+              <Heading as="h3" size="lg" color="yellow.200">Efficient Tip Calculations</Heading>
+              <Text fontSize="lg" m={2}>Calculate tips for your employees quickly and accurately.</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg" color="yellow.200">Employee Management</Heading>
+              <Text fontSize="lg" m={2}>Manage your employees' records, ensuring their tip history is always up-to-date.</Text>
+            </Box>
+            <Box>
+              <Heading as="h3" size="lg" color="yellow.200">Reports</Heading>
+              <Text fontSize="lg" m={2}>Generate comprehensive reports for individual employees or the whole team.</Text>
+            </Box>
+          </VStack>
+        </Box>
+      </Box>
+    </VStack>
+  )
 }
 
 export default Home;
