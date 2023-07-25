48bdf12c152973863d940f0e247dd5d936d61c5f
Josh Sandquist <joshdsand@gmail.com>
2023-07-24 17:58:34 -0700

    Header component testing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,55 @@
-import { Box, Heading, Link as ChakraLink } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Box, Heading, Flex, Link as ChakraLink } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <Box as="header" p={6} bg="blue.500" color="white">
-      <Heading as="h1" size="lg" fontWeight="bold">Employee Tip Calculator</Heading>
-      <Box as="nav" display="flex" gap={4}>
-        <ChakraLink as={Link} to="/" textDecoration="underline" _hover={{textDecoration: "none"}}>Home</ChakraLink>
-        <ChakraLink as={Link} to="/reports" textDecoration="underline" _hover={{textDecoration: "none"}}>Reports</ChakraLink>
-        <ChakraLink as={Link} to="/employees" textDecoration="underline" _hover={{textDecoration: "none"}}>Employees</ChakraLink>
+    <Flex 
+      as="header" 
+      p={6} 
+      bg="blue.700" 
+      color="white" 
+      alignItems={{ base: "center"}}
+      justifyContent={{ base: "center", md: "space-between" }} 
+      flexDirection={{ base: "column", md: "row" }}
+    >
+      <Heading as="h1" size="xl" p={2} fontWeight="bold">Employee Tip Calculator</Heading>
+      <Box as="nav" display="flex" gap={8} p={2} >
+        <ChakraLink 
+          as={NavLink} 
+          to="/" 
+          borderBottom="2px solid white" 
+          fontSize="lg" 
+          _hover={{ borderBottomColor: "transparent" }} 
+          transition="border-bottom-color 0.3s ease"
+          activeStyle={{ color: 'yellow.300' }}
+          exact
+        >
+          Home
+        </ChakraLink>
+        <ChakraLink 
+          as={NavLink} 
+          to="/reports" 
+          borderBottom="2px solid white" 
+          fontSize="lg" 
+          _hover={{ borderBottomColor: "transparent" }} 
+          transition="border-bottom-color 0.3s ease"
+          activeStyle={{ color: 'yellow.300' }}
+        >
+          Reports
+        </ChakraLink>
+        <ChakraLink 
+          as={NavLink} 
+          to="/employees" 
+          borderBottom="2px solid white" 
+          fontSize="lg" 
+          _hover={{ borderBottomColor: "transparent" }} 
+          transition="border-bottom-color 0.3s ease"
+          activeStyle={{ color: 'yellow.300' }}
+        >
+          Employees
+        </ChakraLink>
       </Box>
-    </Box>
+    </Flex>
   );
 }
 
