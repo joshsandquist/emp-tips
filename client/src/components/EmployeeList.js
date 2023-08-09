8027ef2df423d392e183e7323f28f569f607de20
Josh Sandquist <joshdsand@gmail.com>
2023-08-08 17:18:01 -0700

    integrated delete button into employee list

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { Box, Text, VStack, Heading } from '@chakra-ui/react';
+import { useQuery, useMutation, gql } from '@apollo/client';
+import { Box, Text, VStack, Heading, Button } from '@chakra-ui/react';
 
 const GET_EMPLOYEES = gql`
   query GetEmployees {
@@ -11,35 +11,57 @@ const GET_EMPLOYEES = gql`
       hoursWorked
     }
   }
-`;
+`
+const DELETE_EMPLOYEE = gql`
+  mutation DeleteEmployee($id: ID!) {
+    deleteEmployee(id: $id) {
+      id
+    }
+  }
+`;;
 
 function EmployeeList() {
-  const { loading, error, data } = useQuery(GET_EMPLOYEES);
-
-  if (loading) return <Text>Loading...</Text>;
-  if (error) return <Text>Error: {error.message}</Text>;
-
-  return (
-    <VStack spacing={4} align="start" width="full">
-      <Heading as="h2" size="lg">Employees</Heading>
-      {data.getEmployees.map((employee) => (
-        <Box 
-          key={employee.id} 
-          borderWidth="1px" 
-          borderRadius="lg" 
-          padding="4" 
-          width="full"
-          bg="white"
-          boxShadow="md"
-        >
-          <Text fontSize="lg" fontWeight="bold">
-            {employee.firstName} {employee.lastName}
-          </Text>
-          <Text mt={2}>Hours Worked: {employee.hoursWorked}</Text>
-        </Box>
-      ))}
-    </VStack>
-  );
-}
-
-export default EmployeeList;
+    const { loading, error, data, refetch } = useQuery(GET_EMPLOYEES);
+    const [deleteEmployee] = useMutation(DELETE_EMPLOYEE);
+  
+    const handleDelete = async (id) => {
+      try {
+        await deleteEmployee({
+          variables: { id }
+        });
+        refetch();
+      } catch (err) {
+        console.error("Error deleting employee:", err.message);
+      }
+    }
+  
+    if (loading) return <Text>Loading...</Text>;
+    if (error) return <Text>Error: {error.message}</Text>;
+  
+    return (
+      <VStack spacing={4} align="start" width="full">
+        <Heading as="h2" size="lg">Employees</Heading>
+        {data.getEmployees.map((employee) => (
+          <Box 
+            key={employee.id} 
+            borderWidth="1px" 
+            borderRadius="lg" 
+            padding="4" 
+            width="full"
+            bg="white"
+            boxShadow="md"
+          >
+            <Text fontSize="lg" fontWeight="bold">
+              {employee.firstName} {employee.lastName}
+            </Text>
+            <Text mt={2}>Hours Worked: {employee.hoursWorked}</Text>
+            <Button mt={2} colorScheme="red" size="sm" onClick={() => handleDelete(employee.id)}>
+              Delete
+            </Button>
+          </Box>
+        ))}
+      </VStack>
+    );
+  }
+  
+  export default EmployeeList;
